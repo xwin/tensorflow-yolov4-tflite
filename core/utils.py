3703abecd53c2a41f3506861be943f4fb7dffe97
Alex Pelts <alex.pelts@broadcom.com>
2022-07-14 16:48:04 -0700

    Do not overwrite coordinates as a side effect

--- core/utils.py
+++ core/utils.py
@@ -138,7 +138,7 @@ def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_la
     out_boxes, out_scores, out_classes, num_boxes = bboxes
     for i in range(num_boxes[0]):
         if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
-        coor = out_boxes[0][i]
+        coor = np.copy(out_boxes[0][i])
         coor[0] = int(coor[0] * image_h)
         coor[2] = int(coor[2] * image_h)
         coor[1] = int(coor[1] * image_w)
